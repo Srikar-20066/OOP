60c83761b5924b6f041db7eb15808aef26c6eed6
Ch.SaiSrikar <109496058+Srikar-20066@users.noreply.github.com>
2025-07-27 23:08:46 +0530

    Add files via upload

---
+++ Basic Programs Week-1/ExpressionEvaluation.java
@@ -0,0 +1,15 @@
+package week1;
+public class ExpressionEvaluation {
+    public static void main(String[] args) {
+        int expr1 = 10 + 2 * 5; 
+        int expr2 = (10 + 2) * 5; 
+        int expr3 = 100 / 5 + 2 * 3 - 4; 
+        System.out.println("--- Expression Results ---");
+        System.out.println("10 + 2 * 5       = " + expr1); 
+        System.out.println("(10 + 2) * 5     = " + expr2);  
+        System.out.println("100 / 5 + 2 * 3 - 4 = " + expr3); 
+    }
+}
+
+		
+
