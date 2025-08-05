a498cdd3306e90becad0ddc536396c0eb01c3850
Ch.SaiSrikar <109496058+Srikar-20066@users.noreply.github.com>
2025-08-05 14:32:32 +0530

    Update String_buffer.java

--- Practical Task 2/String_buffer.java
+++ Practical Task 2/String_buffer.java
@@ -4,10 +4,11 @@ public class String_buffer {
 
 	public static void main(String[] args) {
 		StringBuffer sb=new StringBuffer("Hello ");
-		   sb.insert(1,"Java");//now original string is changed  
-		   System.out.println(sb);//prints HJavaello  
+		   sb.insert(1,"Java"); 
+		   System.out.println(sb);
 
 
 	}
 
 }
+
